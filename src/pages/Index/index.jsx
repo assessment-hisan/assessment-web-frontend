5503ec0d2b5ee4588c7701007a090e095777e83f
assessment-hisan <assessment.hisan@gmail.com>
2024-09-22 14:08:53 +0530

    Update index.jsx

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 
-const index = () => {
+const Index = () => {
   return (
     <>
     <div className="bg-gray-300 flex items-center justify-between px-6 py-2 drop-shadow">
