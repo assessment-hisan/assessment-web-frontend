b477234e482199680e53221e924595a5648fa47c
assessment-hisan <assessment.hisan@gmail.com>
2024-09-22 13:33:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import { BrowserRouter as Router , Route, Routes } from "react-router-dom"
-import Home from "./pages/Home/Home"
-import SignUp from "./pages/SignUp/SignUp"
-import Login from "./pages/Login/Login"
-import Index from "./pages/Index/index"
-import Admin from  "./pages/Home/Admin"
-const routes = (
-  <Router>
-    <Routes>
-      <Route path="/" exact element={<Index/>}/>
-      <Route path="/admin-dashboard"  exact element={<Admin/>}/>
-      <Route path="/dashboard"  exact element={<Home/>}/>
-      <Route path="/login" exact element={<Login/>}/>
-      <Route path="/signup" exact element={<SignUp/>}/>
-    </Routes>
-  </Router>
-)
+
+import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import SignUp from "./pages/SignUp/SignUp";
+import Login from "./pages/Login/Login";
+import Index from "./pages/Index/index";
+import Admin from "./pages/Home/Admin";
 
 const App = () => {
+ 
+  const isAuthenticated = () => {
+    return !!localStorage.getItem("token"); // Returns true if token exists
+  };
+  
   return (
-  <>{routes}
-  </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={isAuthenticated ? <Home/> : <Index />} />
+        <Route path="/admin-dashboard" element={ <Admin />} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={ <SignUp />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
